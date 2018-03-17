3f4efc908fa82b1767c174e33aee53ef2195e783
Maxim Khanin <maksimkhanin.kpi@gmail.com>
2018-03-17 23:11:35 +0200

    Task1 in Internship

---
+++ Task1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine(Solution());
+        }
+
+        public static int Solution(int target = 200)
+        {
+            var coinValues = new int[] { 1, 2, 5, 10, 20, 50, 100, 200 };
+            var result = new int[target + 1];
+            result[0] = 1;
+
+            for (int i = 0; i < coinValues.Length; i++)
+            {
+                for (int j = coinValues[i]; j <= target; j++)
+                {
+                    result[j] += result[j - coinValues[i]];
+                }
+            }
+
+            return result[target];
+        }
+
+
+
+    }
+
+}
